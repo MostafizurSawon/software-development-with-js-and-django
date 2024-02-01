51560950a2d92f4d7963ff98d45792c00b95f374
Mostafizur06 <mostafizur1102@gmail.com>
2024-02-01 12:57:44 +0600

    revise done

--- 3-intro-to-django/10-working-with-django-templete/simple_page_navigator/navigator/views.py
+++ 3-intro-to-django/10-working-with-django-templete/simple_page_navigator/navigator/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
 
 def about(request):
-    return render(request, 'navigator/about.html')
+    person = {'name':'Md Mostafizur Rahman', 'age': 28, 'date':datetime.datetime.now(), 'district': 'Natore', 'rolls':[4,3,2], 'courses':[
+        {
+            'id':1,
+            'name': 'Python',
+            'des': 'Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation.',
+            'fee': 3000
+
+        },
+        {
+            'id':2,
+            'name': 'OOP with Python',
+            'des': 'Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation.',
+            'fee': 4000
+
+        },
+        {
+            'id':3,
+            'name': 'Django with Python',
+            'des': 'Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation.',
+            'fee': 8000
+
+        }
+
+    ]}
+    return render(request, 'navigator/about.html', person)
 
 def contact(request):
     return render(request, 'navigator/contact.html')
